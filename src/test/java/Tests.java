5c7483c9ce77b7499b73245a087847a0d76eb5b7
Pawel <pkom98@wp.pl>
2023-12-15 16:23:57 +0100

    Added new test cases, TC: 7, 10, 11, 45, 46, 47, 38, 39, 13, 8, 41, 42, 14, 9, 34, 49

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -100,10 +100,204 @@ public class Tests {
         loginPage.openEasybooks();
         loginPage.goToRegistrationForm();
         registrationPage.fillRegistrationForm("Jack", "Jones", "specialcharemail!@gmail.com", "Tester123!");
-        registrationPage.checkIfMessageFailEmail("Niestety, możesz użyć tylko liter (a-z), cyfr (0-9) i kropki (.)");
+        registrationPage.submitRegistrationWithMouseClick();
+        registrationPage.checkIfMessageFailOnRegistration("Niestety, możesz użyć tylko liter (a-z), cyfr (0-9) i kropki (.)");
+
+
+    } //fail until Radek makes changes on easybooks, implementing validation on the Email textbox
+
+    @Test
+    public void TC7(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationForm("firstname", "lastname", "test@gmail.com", "Tester123!");
+        registrationPage.submitRegistrationWithMouseClick();
+        registrationPage.checkIfMessageFailOnRegistration("Użytkownik z podanym adresem email już istnieje.");
+    }
+
+    @Test
+    public void TC10(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationForm("Jack", "Potter", "plznakemailł@gmail.com", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Niestety, możesz użyć tylko liter (a-z), cyfr (0-9) i kropki (.).");
+    }
+
+    @Test
+    public void TC11(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationForm("Harry", "Potter", "baddformat.com", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać poprawny adres e-mail.");
+    }
+
+    @Test
+    public void TC45(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley", "Tester!!!");
+        registrationPage.clickOutsideOfFieldToGetValidation();
+        registrationPage.checkIfMessageFailOnRegistration("Hasło musi zawierać co najmniej 8 znaków w tym jedną dużą literę, cyfrę oraz znak specjalny.");
+    }
+
+    @Test
+    public void TC46(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley", "Tester1234");
+        registrationPage.clickOutsideOfFieldToGetValidation();
+        registrationPage.checkIfMessageFailOnRegistration("Hasło musi zawierać co najmniej 8 znaków w tym jedną dużą literę, cyfrę oraz znak specjalny.");
+    }
+
+    @Test
+    public void TC47(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley", "tester123!");
+        registrationPage.clickOutsideOfFieldToGetValidation();
+        registrationPage.checkIfMessageFailOnRegistration("Hasło musi zawierać co najmniej 8 znaków w tym jedną dużą literę, cyfrę oraz znak specjalny.");
+    }
+
+    @Test
+    public void TC38(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron!", "Weasley", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe imię.");
+    }
+
+    @Test
+    public void TC39(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron whitechar", "Weasley", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe imię.");
+    }
+
+    @Test
+    public void TC13(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ronnumbers12", "Weasley", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe imię.");
+    }
+
+    @Test
+    public void TC8(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ronplsignąę", "Weasley", "Tester123!");
+        registrationPage.submitRegistrationWithMouseClick();
+        registrationPage.goToLogin();
 
 
     }
+
+    @Test
+    public void TC41(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley!", "Tester123");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe nazwisko.");
+    }
+
+    @Test
+    public void TC42(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley whitechar", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe nazwisko.");
+    }
+
+    @Test
+    public void TC14(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasley123", "Tester123!");
+        registrationPage.checkIfMessageFailOnRegistration("Proszę podać prawidłowe nazwisko.");
+    }
+
+    @Test
+    public void TC9(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        RegistrationPage registrationPage = new RegistrationPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.goToRegistrationForm();
+        registrationPage.fillRegistrationFormWithRandomEmail("Ron", "Weasleyąę", "Tester123!");
+        registrationPage.submitRegistrationWithMouseClick();
+        registrationPage.goToLogin();
+    }
+
+    @Test
+    public void TC34(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        AdminPanelPage adminPanelPage = new AdminPanelPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.login("jadmin@gmail.com", "jadmin");
+        adminPanelPage.goToUsersTabAlternative();
+        adminPanelPage.goToAddNewEmployeeForm();
+        adminPanelPage.fillRegistrationFormWithRandomEmailAdmin("First", "Last", "Tester123!");
+        adminPanelPage.submitRegistrationWithMouseClickAdmin();
+        adminPanelPage.messageAfterRegister("Użytkownik został dodany.");
+
+
+
+    }
+
+    @Test
+    public void TC49(){
+        LoginPage loginPage = new LoginPage(driver, wait);
+        AdminPanelPage adminPanelPage = new AdminPanelPage(driver, wait);
+
+        loginPage.openEasybooks();
+        loginPage.login("jadmin@gmail.com", "jadmin");
+        adminPanelPage.goToUsersTabAlternative();
+        adminPanelPage.goToAddNewEmployeeForm();
+        adminPanelPage.fillRegistrationFormWithRandomEmailWithSpecialCharAdmin("Firstname", "Lastname", "Tester123!");
+        adminPanelPage.submitRegistrationWithMouseClickAdmin();
+        adminPanelPage.messageAfterRegister("Użytkownik został dodany.");
+    } //This should have an error message after submitting registration, to be checked and rewritten after fixes from dev
 
 //    @After
 //    public void isItOK() {
